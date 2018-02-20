ad1da4f529af92b731ccff0690a9465ab00e7977
Joarhal <joarhal@gmail.com>
2018-02-21 02:32:08 +0500

    fix

---
+++ vp/3.3845d0b4569b0252886c.bundle.js
@@ -0,0 +1,19 @@
+webpackJsonp([3],{
+
+/***/ "./src/config/app_settings/fb.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = {
+  resolution: {
+    width: 940,
+    height: 760
+  }
+};
+
+/***/ })
+
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvY29uZmlnL2FwcF9zZXR0aW5ncy9mYi5qcyJdLCJuYW1lcyI6WyJtb2R1bGUiLCJleHBvcnRzIiwicmVzb2x1dGlvbiIsIndpZHRoIiwiaGVpZ2h0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBQSxPQUFPQyxPQUFQLEdBQWlCO0FBQ2ZDLGNBQVk7QUFDVkMsV0FBTyxHQURHO0FBRVZDLFlBQVE7QUFGRTtBQURHLENBQWpCLEMiLCJmaWxlIjoiMy4zODQ1ZDBiNDU2OWIwMjUyODg2Yy5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IHtcbiAgcmVzb2x1dGlvbjoge1xuICAgIHdpZHRoOiA5NDAsXG4gICAgaGVpZ2h0OiA3NjBcbiAgfVxufVxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vc3JjL2NvbmZpZy9hcHBfc2V0dGluZ3MvZmIuanMiXSwic291cmNlUm9vdCI6IiJ9
