1e1fbf15ff87ee8fe2705b1e53436ce9a40a81b3
Joarhal <joarhal@gmail.com>
2018-02-21 00:36:23 +0500

    add vp

---
+++ vp/0.230410e21f32c9116f01.bundle.js
@@ -0,0 +1,30 @@
+webpackJsonp([0],{
+
+/***/ "./src/screens/Game.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__("./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return _react2.default.createElement(
+    'div',
+    null,
+    'Game'
+  );
+};
+
+/***/ })
+
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvc2NyZWVucy9HYW1lLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7OztBQUFBOzs7Ozs7a0JBRWU7QUFBQSxTQUNiO0FBQUE7QUFBQTtBQUFBO0FBQUEsR0FEYTtBQUFBLEMiLCJmaWxlIjoiMC4yMzA0MTBlMjFmMzJjOTExNmYwMS5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QsIHtQdXJlQ29tcG9uZW50fSBmcm9tICdyZWFjdCc7XG5cbmV4cG9ydCBkZWZhdWx0ICgpID0+IChcbiAgPGRpdj5HYW1lPC9kaXY+XG4pXG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9zcmMvc2NyZWVucy9HYW1lLmpzIl0sInNvdXJjZVJvb3QiOiIifQ==
