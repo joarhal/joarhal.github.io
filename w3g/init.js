9dd10b18395194b8dce0d84ad98d2ad79d2b7fcc
Nikita Kislitsin <joarhal@gmail.com>
2024-08-04 17:06:41 +0200

    update

--- w3g/init.js
+++ w3g/init.js
@@ -1,9 +1,8 @@
-(async () => {
-  console.log('init.js');
+const init = async () => {
+  console.log("init.js");
   const WebApp = window.Telegram?.WebApp;
   const W3G_SERVER = "https://0uitsxjg97.whs.lol/";
 
-  
   if (WebApp) {
     const theme = WebApp.themeParams;
     if (theme?.bg_color) {
@@ -12,10 +11,26 @@
   }
 
   var res = await fetch(W3G_SERVER);
-  console.log('-----');
+  console.log("-----");
   if (res.ok) {
     var data = await res.json();
     console.log(data);
   }
   // WebApp.ready();
+};
+
+(async () => {
+  try {
+    await init();
+  } catch (error) {
+    if (window?.Telegram?.WebApp) {
+      const WebApp = window.Telegram.WebApp;
+      WebApp.show_alert(
+        "Something went wrong, please try again later.\n Error: 6545192"
+      );
+      WebApp.close();
+    } else {
+      alert("Something went wrong, please try again later.\n Error: 6545192");
+    }
+  }
 })();
