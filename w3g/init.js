1b8ae14fcdcfd13f7753f3180e8ebf85e556c0bf
Nikita Kislitsin <joarhal@gmail.com>
2024-08-04 17:41:41 +0200

    update

--- w3g/init.js
+++ w3g/init.js
@@ -3,7 +3,7 @@ const init = async () => {
   const WebApp = window.Telegram?.WebApp;
   const W3G_SERVER = "https://0uitsxjg97.whs.lol/";
 
-  if (WebApp) {
+  if (WebApp && WebApp.themeParams) {
     const theme = WebApp.themeParams;
     if (theme?.bg_color) {
       document.body.style.background = theme.bg_color;
@@ -11,28 +11,28 @@ const init = async () => {
   }
 
   var res = await fetch(W3G_SERVER);
-  console.log("-----");
   if (res.ok) {
     var data = await res.json();
-    console.log(data);
+    if (!data.ok) {
+      failApp("Something went wrong, please try again later.\n Error: 6548147");
+    }
   }
   // WebApp.ready();
+};
+
+const failApp = (message) => {
+  if (window?.Telegram?.WebApp?.init_data) {
+    WebApp.show_alert(message);
+    WebApp.close();
+  } else {
+    alert(message);
+  }
 };
 
 (async () => {
   try {
     await init();
   } catch (error) {
-    if (window?.Telegram?.WebApp) {
-      const WebApp = window.Telegram.WebApp;
-      if (WebApp.init_data) {
-        WebApp.show_alert(
-          "Something went wrong, please try again later.\n Error: 6545192"
-        );
-        WebApp.close();
-      }
-    } else {
-      alert("Something went wrong, please try again later.\n Error: 6545192");
-    }
+    failApp("Something went wrong, please try again later.\n Error: 6545192");
   }
 })();
