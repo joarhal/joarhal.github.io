5dd126e0d97e2591c8a1fd59fb68661e4962f958
Nikita Kislitsin <joarhal@gmail.com>
2024-08-04 16:59:17 +0200

    update

---
+++ w3g/init.js
@@ -0,0 +1,18 @@
+(async () => {
+  console.log('init.js');
+  const WebApp = window.WebApp;
+  const W3G_SERVER = "https://0uitsxjg97.whs.lol/";
+
+  const theme = WebApp.themeParams;
+
+  if (theme.bg_color) {
+    document.body.style.background = theme.bg_color;
+  }
+  var res = await fetch(W3G_SERVER);
+  console.log('-----');
+  if (res.ok) {
+    var data = await res.json();
+    console.log(data);
+  }
+  // WebApp.ready();
+})();
